3c4343ceb34dcbed722af3eca64b31f592c303d9
redaphid <iam@aaronherres.com>
2019-10-08 18:30:28 -0700

    📓

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 pub fn eval(action: String) -> String {
-    if action == "👀" {
+    if action == "👀" || action == "🚪\n🚪" {
         return "🌲🌲🏚🌲🌲".to_string();
     }
     if action == "🚪" {
         return "🛌🛋".to_string();
+    }
+    if action == "🚪\n👏" {
+        return "🛏⛄".to_string()
     }
     return "☠".to_string()
 }
@@ -25,5 +28,14 @@ mod tests {
     #[test]
     fn move_closer_to_the_house() {
         assert_eq!(eval("🚪".to_string()), "🛌🛋");
+    }
+    #[test]
+    fn leave_house() {                
+        assert_eq!(eval("🚪\n🚪".to_string()), "🌲🌲🏚🌲🌲");
+    }
+
+    #[test]
+    fn wake_up_the_guy() {                
+        assert_eq!(eval("🚪\n👏".to_string()), "🛏⛄");
     }
 }
